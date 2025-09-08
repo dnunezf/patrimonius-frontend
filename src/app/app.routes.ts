9d8a416a059915ce3cf6430d551ea2c9870ecf83
Kendra Artavia <kendra.artavia.caballero@gmail.com>
2025-09-08 01:39:58 -0600

    feat login
     Conflicts:
    	src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-
 import { HomeComponent } from '../home/home.component';
 
-
-
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
+  },
   {
     path: '',
     loadComponent: () =>
