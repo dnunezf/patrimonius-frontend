91160a5b5ad5a9a49eab830a3ee2af675be3e85a
Kendra Artavia <kendra.artavia.caballero@gmail.com>
2025-09-09 10:04:51 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
-import { ConsultasBitacorasComponent } from './features/bitacoras/consultas-bitacoras/consultas-bitacoras.component';  // Adjust the import path
+import { ConsultasBitacorasComponent } from './features/bitacoras/consultas-bitacoras/consultas-bitacoras.component'; // Ajusta el path de importación
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },  // Home
   {
     path: 'admin',
     loadChildren: () =>
       import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
   },
   {
-    path: '',
-    loadComponent: () =>
-      import('./features/home/home.component').then(m => m.HomeComponent),
-    title: 'Patrimonius | Inicio',
+    path: 'bitacoras',
+    component: ConsultasBitacorasComponent,
   },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
-  },
-  { path: '**', redirectTo: '' },
-  { path: '', component: HomeComponent },      // Home
-  { path: 'bitacoras', component: ConsultasBitacorasComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },  // Ruta de fallback
 ];
