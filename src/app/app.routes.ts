5fbe14121efaab9e3fd3e969db944b43741c21d0
LittleHehet <alexia.alvarado18@gmail.com>
2025-09-11 18:44:14 -0600

    fix: app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AccessExceptionsComponent } from './features/admin/access-exceptions/access-exceptions.component';
 
-import { ConsultasBitacorasComponent } from './features/bitacoras/consultas-bitacoras/consultas-bitacoras.component';  // Adjust the import path
-import { AccessExceptionsComponent } from './features/admin/access-exceptions/access-exceptions.component';
-
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
@@ -17,13 +14,6 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./features/logs/logs.routes').then(m => m.LOGS_ROUTES),
   },
-  { path: 'access-exceptions', component: AccessExceptionsComponent },
-  {
-    path: 'logs',
-    loadChildren: () =>
-      import('./features/logs/logs.routes').then(m => m.LOGS_ROUTES),
-  },
-  { path: 'bitacoras', component: ConsultasBitacorasComponent },
   { path: 'access-exceptions', component: AccessExceptionsComponent },
   { path: '**', redirectTo: '' }
 ];
