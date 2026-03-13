015548ec552eb05728dea823d2face85d0846901
LittleHehet <alexia.alvarado18@gmail.com>
2026-03-13 00:01:27 -0600

    Validación de Firma

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { AccessExceptionsComponent } from './features/admin/access-exceptions/access-exceptions.component';
 import { ActivateComponent } from './auth/activate.component';
-import { VerifyExternalSignaturePageComponent } from "app/pages/verify-external-signature-page/verify-external-signature-page.component";
-import { VerifyExternalSignatureComponent } from "app/features/firma-externa/verify-external-signature/verify-external-signature.component";
 
 
 export const routes: Routes = [
@@ -66,13 +64,5 @@ export const routes: Routes = [
 
   { path: 'access-exceptions', component: AccessExceptionsComponent },
   { path: 'activate', component: ActivateComponent },
-  {
-    path: "archivo/external-signatures/:documentoId/verify",
-    component: VerifyExternalSignaturePageComponent,
-  },
-  {
-    path: "archivo/external-signatures/:documentoId/verify",
-    component: VerifyExternalSignatureComponent,
-  },
   { path: '**', redirectTo: '' },
 ];
