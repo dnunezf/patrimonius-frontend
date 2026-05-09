10040a621787400408c33e1df7280906f9eba53b
Kendra Artavia Caballero <kendra.artavia.caballero@gmail.com>
2026-05-09 00:15:07 -0600

    environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
-export const environment = {
+/* export const environment = {
   production: false,
   api: 'http://localhost:3000',
   apiUrl: 'http://localhost:3000', // <- alias para servicios antiguos
+  wsPath: '/ws'
+};
+*/
+
+export const environment = {
+  production: false,
+  api: 'https://patrimonius-backend.onrender.com',
+  apiUrl: 'https://patrimonius-backend.onrender.com', // <- alias para servicios antiguos
   wsPath: '/ws'
 };
